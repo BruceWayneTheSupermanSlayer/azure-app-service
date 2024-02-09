4e3847e2b24d25d36ad87082f7c7bf21510e58ed
Rohit Kumar <44352447+BruceWayneTheSupermanSlayer@users.noreply.github.com>
2024-02-09 05:56:57 +0000

    update container name and added Axure Publish profile

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,8 +6,8 @@ import { SeverityLevel } from 'applicationinsights/out/Declarations/Contracts';
 export class AppService {
   constructor(private readonly logger: AppInsightsService) { }
   getHello(): Record<string, string> {
-    this.logger.info(`💪💪💪💪💪💪💪💪💪💪💪💪💪💪`, SeverityLevel.Error)
+    this.logger.info(`👻👻👻👻👻👻👻👻👻👻👻👻👻👻`, SeverityLevel.Error)
 
-    return { message: "💪💪💪💪💪💪💪💪💪💪💪💪💪💪" };
+    return { message: "👻👻👻👻👻👻👻👻👻👻👻👻👻👻👻" };
   }
 }
